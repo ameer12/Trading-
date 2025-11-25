5e42ab4ee4de4a01f3e453ccae16b49b083143a0
ameer12 <fatenhs343@gmail.com>
2025-11-25 11:02:41 +0300

    Create signalEngine.js

---
+++ src/modules/signalEngine.js
@@ -0,0 +1,19 @@
+export function getSignals() {
+  return [
+    {
+      symbol: 'EUR/USD',
+      type: 'buy',
+      reason: 'تقاطع متوسطات إيجابي على إطار 15 دقيقة'
+    },
+    {
+      symbol: 'BTC/USD',
+      type: 'sell',
+      reason: 'زخم سلبي بعد رفض مقاومة 38,000'
+    },
+    {
+      symbol: 'USD/JPY',
+      type: 'buy',
+      reason: 'دعم قوي عند 147.50 + إشارات ارتداد'
+    }
+  ];
+}
